237f521f13b7ee221ffa45bd704701696ba23bd3
Adrianfeloh1 <adrianfeloh1@gmail.com>
2024-04-29 11:11:26 -0500

    Funcionalidad darkMode en mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MoonIcon } from "./icons/MoonIcon";
 import { SunIcon } from "./icons/SunIcon";
 
 export const Header = () => {
-   const [darkMode, setDarkMode] = useState(false);
+   const [darkMode, setDarkMode] = useState(true);
 
-   
+   useEffect(() => {
+      if (darkMode) {
+         document.documentElement.classList.add("dark");
+      } else {
+         document.documentElement.classList.remove("dark");
+      }
+   }, [darkMode]);
 
    return (
       <header className=" container mx-auto px-4 pt-8">
